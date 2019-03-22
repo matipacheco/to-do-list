6b872804b9efa952d3cb21c713ca624b936f6a79
Mati <matipacheco@gmail.com>
2019-03-21 22:19:30 -0300

    one container with teo boards

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
 import '../css/App.css';
+import React, { Component } from 'react';
+
+import {Board} from "./Board";
 
 class App extends Component {
+  renderBoard() {
+    return <Board/>
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        {this.renderBoard()}
+        {this.renderBoard()}
       </div>
     );
   }
---
+++ src/javascript/Board.js
@@ -0,0 +1,12 @@
+import '../css/Board.css';
+import React, { Component } from 'react';
+
+class Board extends Component {
+    render() {
+        return(
+            <div className="board"></div>
+        );
+    }
+}
+
+export {Board};
