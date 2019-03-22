94a286b6e0e74a792d7b40b711894514b56951b0
Mati <matipacheco@gmail.com>
2019-03-22 16:32:10 -0300

    adding the card component

--- src/javascript/Board.js
+++ src/javascript/Board.js
@@ -1,10 +1,14 @@
 import '../css/Board.css';
+import {Card} from './Card'
 import React, { Component } from 'react';
 
 class Board extends Component {
     render() {
         return(
-            <div className="board"></div>
+            <div className="board">
+                <Card/>
+                <Card/>
+            </div>
         );
     }
 }
---
+++ src/javascript/Card.js
@@ -0,0 +1,14 @@
+import '../css/Card.css';
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                Cajita!
+            </div>
+        );
+    }
+}
+
+export {Card}
