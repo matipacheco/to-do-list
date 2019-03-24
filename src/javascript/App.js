fae13bd3ec14facb3203c4a3d6acab4be061d1f0
Mati <matipacheco@gmail.com>
2019-03-24 19:52:55 -0300

    removing movable cards

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -4,9 +4,6 @@ import React, { Component } from 'react';
 import { Board } from "./Board";
 import {NewCardButton} from "./NewCardButton";
 
-import HTML5Backend from "react-dnd-html5-backend";
-import { DragDropContextProvider } from 'react-dnd';
-
 class App extends Component {
   renderBoard() {
     return (
@@ -17,12 +14,10 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <DragDropContextProvider backend={ HTML5Backend }>
           { this.renderBoard() }
           <div className='new_card_button_section'>
             <NewCardButton/>
           </div>
-        </DragDropContextProvider>
       </div>
     );
   }
--- src/javascript/Card.js
+++ src/javascript/Card.js
@@ -1,28 +1,22 @@
 import '../css/Card.css';
-import React from 'react';
+import React, { Component } from 'react';
 
-import { DragSource } from 'react-dnd';
-import { ItemTypes } from "../utils/Constants";
+// function Card({ connectDragSource, isDragging, task_description }){
+//     return connectDragSource(
+//         <div className="card" style={{opacity: isDragging ? 0.5 : 1, fontSize: 25, cursor: 'move'}}>
+//             { task_description }
+//         </div>,
+//     )
+// }
 
-const cardSource = {
-    beginDrag(props) {
-        return { task_description: props.task_description }
-    }
-};
-
-function collect(connect, monitor) {
-    return {
-        connectDragSource: connect.dragSource(),
-        isDragging: monitor.isDragging(),
+class Card extends Component {
+    render() {
+        return(
+            <div className='card' style={ { fontSize: 25 } }>
+                { this.props.task_description }
+            </div>
+        )
     }
 }
 
-function Card({ connectDragSource, isDragging, task_description }){
-    return connectDragSource(
-        <div className="card" style={{opacity: isDragging ? 0.5 : 1, fontSize: 25, cursor: 'move'}}>
-            { task_description }
-        </div>,
-    )
-}
-
-export default DragSource(ItemTypes.CARD, cardSource, collect)(Card);
+export { Card };
