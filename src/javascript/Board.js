c5cddbe4e56ad57fd04344a98c128f302f78bd36
Mati <matipacheco@gmail.com>
2019-03-24 16:49:39 -0300

    connecting to API, but CORS fuck everything up

--- src/javascript/Board.js
+++ src/javascript/Board.js
@@ -2,8 +2,18 @@ import '../css/Board.css';
 import Card from './Card'
 import React, { Component } from 'react';
 
+import axios from 'axios';
+import {GetAllTasksUrl} from "../utils/Constants";
+
+function getItems() {
+    axios.get(GetAllTasksUrl)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+}
+
 class Board extends Component {
     render() {
+        getItems();
         return(
             <div className="board">
                 <Card content={ 'Hello' }/>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,5 @@
 export const ItemTypes = {
     CARD: 'card',
-}
+};
 
+export const GetAllTasksUrl = 'https://dry-tundra-97096.herokuapp.com/to-do-list/api/tasks';
