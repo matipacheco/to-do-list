1f709298b50622b1bea6a2d0b41ad514caca36e5
Mati <matipacheco@gmail.com>
2019-03-24 19:53:34 -0300

    this was mossing its removal

--- src/javascript/Card.js
+++ src/javascript/Card.js
@@ -1,14 +1,6 @@
 import '../css/Card.css';
 import React, { Component } from 'react';
 
-// function Card({ connectDragSource, isDragging, task_description }){
-//     return connectDragSource(
-//         <div className="card" style={{opacity: isDragging ? 0.5 : 1, fontSize: 25, cursor: 'move'}}>
-//             { task_description }
-//         </div>,
-//     )
-// }
-
 class Card extends Component {
     render() {
         return(
