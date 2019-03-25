933724cdc88ab0862618651b8179123f330aef3c
Mati <matipacheco@gmail.com>
2019-03-25 20:33:25 -0300

    minor code refactor

--- src/javascript/Board.js
+++ src/javascript/Board.js
@@ -1,10 +1,8 @@
 import '../css/Board.css';
 import { Card } from './Card';
 import React, { Component } from 'react';
-import {NewCardButton} from './NewCardButton'
-
-// import axios from 'axios';
-// import { GetAllTasksUrl } from '../utils/Constants';
+import { NewCardButton } from './NewCardButton';
+import { getMockedItems } from "../utils/utils";
 
 class Board extends Component {
     constructor(props) {
@@ -34,25 +32,7 @@ class Board extends Component {
     };
 
     getItems() {
-        // Since CORS are fucking everything up.... This a temporary solution
-        let payload = require('../utils/mocks/get_all_tasks_mock').payload;
-        let items   = payload ? payload : [];
-        return items;
-
-        // let headers = new Headers({ 'Access-Control-Allow-Origin': '*' });
-        // let config  = {
-        //     method: "GET",
-        //     headers: headers,
-        //     mode: "cors"
-        // };
-        //
-        // fetch(GetAllTasksUrl, config)
-        //     .then(response => console.log(response.json()))
-        //     .catch(error => console.log(error));
-
-        // axios.get(GetAllTasksUrl, { headers: { ''Access-Control-Allow-Origin': '*' } })
-        //     .then(data => console.log(data))
-        //     .catch(err => console.log(err));
+        return getMockedItems();
     }
 
     renderItems = () => {
@@ -66,7 +46,7 @@ class Board extends Component {
     render() {
         return(
             <div className='board_section'>
-                <NewCardButton addCard={this.addCard} />
+                <NewCardButton addCard={ this.addCard } />
                 { this.renderItems() }
             </div>
         );
---
+++ src/utils/utils.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { GetAllTasksUrl } from './Constants';
+
+function getItems() {
+  // let headers = new Headers({ 'Access-Control-Allow-Origin': '*' });
+  // let config  = {
+  //     method: "GET",
+  //     headers: headers,
+  //     mode: "cors"
+  // };
+  //
+  // fetch(GetAllTasksUrl, config)
+  //     .then(response => console.log(response.json()))
+  //     .catch(error => console.log(error));
+  axios.get(GetAllTasksUrl, {headers: {'Access-Control-Allow-Origin': '*'}})
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+}
+
+function getMockedItems() {
+  // Since CORS are fucking everything up.... This a temporary solution
+  let payload = require('./mocks/get_all_tasks_mock').payload;
+  return (payload ? payload : []);
+
+}
+
+export { getItems, getMockedItems }
