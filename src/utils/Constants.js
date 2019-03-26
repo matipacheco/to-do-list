3cae470e83bd5ec4ea6e0c936c53372db26411ee
Mati <matipacheco@gmail.com>
2019-03-26 09:51:06 -0300

    json server url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,2 @@
-export const ItemTypes = {
-    CARD: 'card',
-};
-
-// export const GetAllTasksUrl = 'http://localhost:3001/to-do-list/api/tasks';
+export const GetAllTasksJsonServerUrl = 'http://localhost:3001/payload';
 export const GetAllTasksUrl = 'https://dry-tundra-97096.herokuapp.com/to-do-list/api/tasks';
